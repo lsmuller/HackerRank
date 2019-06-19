4bfb38af1d87c2430d3826f90df45a181b45052f
Leticia Muller <leticia.muller@sap.com>
2019-06-19 06:13:46 -0300

    daily

---
+++ src/com/leticia/MarkAndToys.java
@@ -0,0 +1,23 @@
+package com.leticia;
+
+import java.util.Arrays;
+
+public class MarkAndToys {
+
+	public static int maximumToys(int[] prices, int k) {
+		Arrays.sort(prices);
+		int totalPrice = 0;
+		int i;
+		for (i = 0; i < prices.length; i++) {
+			totalPrice += prices[i];
+			if (totalPrice >= k) break;
+		}
+		return totalPrice > 0 ? i : 0;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = {1, 12, 5, 111, 200, 1000, 10};
+		System.out.println(maximumToys(arr, 50));
+	}
+
+}
